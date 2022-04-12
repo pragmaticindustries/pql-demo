51a85ec5bdd425f96cdbb0a1acdf2af4661ee10b
julian <j.feinauer@pragmaticminds.de>
2022-04-12 08:15:35 +0200

    Push new (failing) test

--- test_extract.py
+++ test_extract.py
@@ -35,7 +35,6 @@ def test_one():
     ]
 
     results = run_processor(states)
-    print(results,flush=True)
 
     assert len(results.get("Cycle")) == 2
     assert len(results.get("MaterialEquipped")) == 1
@@ -59,7 +58,6 @@ def test_two():
         processor.process_state(state)
 
     results = processor.get_result()
-    print(results,flush=True)
 
     assert len(results.get("Cycle")) == 2
     assert len(results.get("MaterialEquipped")) == 1
@@ -69,3 +67,27 @@ def test_two():
     assert results.get("Cycle")[1].get("material_equipped") == 1
 
 
+def test_cycle_material_switch():
+    states = [
+        {"timestamp": 1, "cycle_id": 21, "material_name": "Material 1"},
+        {"timestamp": 2, "cycle_id": 21, "material_name": "Material 1"},
+        {"timestamp": 3, "cycle_id": 22, "material_name": "Material 2"},
+        {"timestamp": 4, "cycle_id": 22, "material_name": "Material 2"},
+    ]
+
+    processor = StateProcessor(config,states[0])
+    processor.reset_fk_constraints_and_autogen_fields()
+    for state in states:
+        processor.process_state(state)
+
+    results = processor.get_result()
+
+    assert len(results.get("Cycle")) == 2
+    assert len(results.get("MaterialEquipped")) == 2
+
+    assert results.get("MaterialEquipped")[0] == {'end': 2, 'id': 1, 'material_name': 'Material 1', 'start': 1}
+    assert results.get("MaterialEquipped")[1] == {'end': 4, 'id': 1, 'material_name': 'Material 2', 'start': 3}
+
+    assert results.get("Cycle")[0] == {'start': 1, 'end': 2, 'id': 21, 'material_equipped': 1}
+    # TODO Why does this test fail?
+    assert results.get("Cycle")[1] == {'start': 3, 'end': 4, 'id': 22, 'material_equipped': 2}
