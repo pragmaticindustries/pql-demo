a373cbc76c7e38fb0814ef1844172688c3134bf1
Simon Mayer <40806645+pragmayticer@users.noreply.github.com>
2022-04-13 13:34:18 +0200

    Create pre-commit.yml (#14)
    Update pre-commit.yml
    
    Rename pre-commit.yml to pre-commit-config.yml
    
    Update requirements.txt
    
    Update requirements.txt
    
    Update pre-commit-config.yml
    
    Update pre-commit-config.yml
    
    Update pre-commit-config.yml
    
    Update pre-commit-config.yml
    
    tryed to run black and flake8 in pre-commit
    
    added pre-commit file
    
    Create pre-commit.yml
    
    Update pre-commit.yml
    
    Rename pre-commit.yml to pre-commit-config.yml
    
    Update requirements.txt
    
    Update requirements.txt
    
    Update pre-commit-config.yml
    
    Update pre-commit-config.yml
    
    Update pre-commit-config.yml
    
    Update pre-commit-config.yml
    
    addd pylint
    
    try to add pre-commit in githubworkflows directory
    
    deleted it
    
    ran black
    
    Update pylint.yml

--- test_groupby.py
+++ test_groupby.py
@@ -5,7 +5,15 @@ from random import uniform
 from typing import Iterable
 
 from main import get_all_assets
-from pql import EntityContext, agg_functions, RootContext, Query, Projection, Aggregation, SubQuery
+from pql import (
+    EntityContext,
+    agg_functions,
+    RootContext,
+    Query,
+    Projection,
+    Aggregation,
+    SubQuery,
+)
 
 random.seed(123)
 
@@ -16,7 +24,14 @@ def create_cycles(n):
     for i in range(0, n):
         cycle_duration = timedelta(seconds=uniform(20.0, 30.0))
         pause = timedelta(seconds=uniform(1.0, 60.0))
-        cycles.append({"id": i, "start": timestamp, "end": timestamp + cycle_duration, "machine": "LHL 01"})
+        cycles.append(
+            {
+                "id": i,
+                "start": timestamp,
+                "end": timestamp + cycle_duration,
+                "machine": "LHL 01",
+            }
+        )
 
         timestamp = timestamp + cycle_duration + pause
 
@@ -29,8 +44,15 @@ def create_tools(n):
     for i in range(0, n):
         duration = timedelta(minutes=uniform(20.0, 40.0))
         pause = timedelta(minutes=uniform(5.0, 15.0))
-        tools.append({"id": uuid.uuid4(), "name": f"Tool {i}", "start": timestamp, "end": timestamp + duration,
-                      "machine": "LHL 01"})
+        tools.append(
+            {
+                "id": uuid.uuid4(),
+                "name": f"Tool {i}",
+                "start": timestamp,
+                "end": timestamp + duration,
+                "machine": "LHL 01",
+            }
+        )
 
         timestamp = timestamp + duration + pause
 
@@ -44,8 +66,14 @@ def create_materials(n):
         duration = timedelta(minutes=uniform(10.0, 20.0))
         pause = timedelta(minutes=uniform(0.0, 5.0))
         materials.append(
-            {"id": uuid.uuid4(), "material": f"Material {i % 2}", "start": timestamp, "end": timestamp + duration,
-             "machine": "LHL 01"})
+            {
+                "id": uuid.uuid4(),
+                "material": f"Material {i % 2}",
+                "start": timestamp,
+                "end": timestamp + duration,
+                "machine": "LHL 01",
+            }
+        )
 
         timestamp = timestamp + duration + pause
 
@@ -69,19 +97,23 @@ def get_all_assets(name: str) -> Iterable[dict]:
 
 
 class InMemoryAssetRetriever:
-
     def get_assets(self, asset_type, where_clause, group_by_clause):
         assets = get_all_assets(asset_type)
         if where_clause:
             assets = [o for o in assets if where_clause(o)]
         if group_by_clause:
+
             def get_group_for_object(o):
-                return tuple([p.execute(EntityContext(None, o)) for p in group_by_clause])
+                return tuple(
+                    [p.execute(EntityContext(None, o)) for p in group_by_clause]
+                )
 
             groups = list({get_group_for_object(o) for o in assets})
 
             # Now return the assets in chunks
-            assets = [[o for o in assets if get_group_for_object(o) == g] for g in groups]
+            assets = [
+                [o for o in assets if get_group_for_object(o) == g] for g in groups
+            ]
         return assets
 
 
@@ -90,19 +122,37 @@ context = RootContext(asset_retriever.get_assets, lambda s: agg_functions.get(s)
 
 
 def test_query():
-    query = Query([
-        Projection("name"),
-        Aggregation("count", Query([Projection("*")], "Cycles"), name="cycles"),
-        Aggregation("flatten", Query([Projection("material")], "Materials"), name="products"),
-        SubQuery(
-            Query([
-                Projection("material"), Aggregation("count", Query([Projection("*")], "Cycles"), name="cycles")
-            ], "Materials"), name="material_and_count")
-    ], "Tools")
+    query = Query(
+        [
+            Projection("name"),
+            Aggregation("count", Query([Projection("*")], "Cycles"), name="cycles"),
+            Aggregation(
+                "flatten", Query([Projection("material")], "Materials"), name="products"
+            ),
+            SubQuery(
+                Query(
+                    [
+                        Projection("material"),
+                        Aggregation(
+                            "count", Query([Projection("*")], "Cycles"), name="cycles"
+                        ),
+                    ],
+                    "Materials",
+                ),
+                name="material_and_count",
+            ),
+        ],
+        "Tools",
+    )
 
     results = query.execute(context)
 
-    assert results[0] == {'cycles': 24, 'name': 'Tool 0', 'material_and_count': [
-        {'cycles': 21, 'material': 'Material 0'},
-        {'cycles': 19, 'material': 'Material 1'}],
-                          'products': ['Material 0', 'Material 1']}
+    assert results[0] == {
+        "cycles": 24,
+        "name": "Tool 0",
+        "material_and_count": [
+            {"cycles": 21, "material": "Material 0"},
+            {"cycles": 19, "material": "Material 1"},
+        ],
+        "products": ["Material 0", "Material 1"],
+    }
